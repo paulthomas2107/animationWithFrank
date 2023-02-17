f9750320a037a8edb80aecfec1418434f0be550c
Paul Thomas <paulthomas2107@gmail.com>
2023-02-17 13:46:41 +0000

    More

--- script.js
+++ script.js
@@ -18,24 +18,29 @@ class Line {
     ];
     this.lineWidth = Math.floor(Math.random() * 15 + 1);
     this.hue = Math.floor(Math.random() * 360);
+    this.maxLength = 10;
   }
   draw(context) {
     context.strokeStyle = 'hsl(' + this.hue + ', 100%, 50%)';
     context.lineWidth = this.lineWidth;
     context.beginPath();
     context.moveTo(this.history[0].x, this.history[0].y);
-    for (let i = 0; i < 3; i++) {
-      this.x = Math.random() * canvas.width;
-      this.y = Math.random() * canvas.height;
-      this.history.push({
-        x: this.x,
-        y: this.y,
-      });
-    }
+
     for (let i = 0; i < this.history.length; i++) {
       context.lineTo(this.history[i].x, this.history[i].y);
     }
     context.stroke();
+  }
+  update() {
+    this.x = Math.random() * canvas.width;
+    this.y = Math.random() * canvas.height;
+    this.history.push({
+      x: this.x,
+      y: this.y,
+    });
+    if (this.history.length > this.maxLength) {
+      this.history.shift();
+    }
   }
 }
 
@@ -44,4 +49,14 @@ const numberOfLines = 1;
 for (let i = 0; i < numberOfLines; i++) {
   linesArray.push(new Line(canvas));
 }
-linesArray.forEach((line) => line.draw(ctx));
+
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  linesArray.forEach((line) => {
+    line.draw(ctx);
+    line.update();
+  });
+  requestAnimationFrame(animate);
+}
+
+animate();
