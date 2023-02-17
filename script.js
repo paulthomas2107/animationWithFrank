8781572f21e762ebe941306c2b2d902d4610242c
Paul Thomas <paulthomas2107@gmail.com>
2023-02-17 14:03:44 +0000

    More

--- script.js
+++ script.js
@@ -20,7 +20,9 @@ class Line {
     this.hue = Math.floor(Math.random() * 360);
     this.maxLength = 10;
     this.speedX = 2;
-    this.speedY = 15;
+    this.speedY = 6;
+    this.lifeSpan = this.maxLength * 10;
+    this.timer = 0;
   }
   draw(context) {
     context.strokeStyle = 'hsl(' + this.hue + ', 100%, 50%)';
@@ -33,16 +35,36 @@ class Line {
     }
     context.stroke();
   }
+
   update() {
-    this.x += this.speedX + Math.random() * 50 - 25;
-    this.y += this.speedY + Math.random() * 50 - 25;
-    this.history.push({
-      x: this.x,
-      y: this.y,
-    });
-    if (this.history.length > this.maxLength) {
+    this.timer++;
+    if (this.timer < this.lifeSpan) {
+      this.x += this.speedX + Math.random() * 50 - 25;
+      this.y += this.speedY + Math.random() * 50 - 25;
+      this.history.push({
+        x: this.x,
+        y: this.y,
+      });
+      if (this.history.length > this.maxLength) {
+        this.history.shift();
+      }
+    } else if (this.history.length <= 1) {
+      this.reset();
+    } else {
       this.history.shift();
     }
+  }
+
+  reset() {
+    this.x = Math.random() * canvas.width;
+    this.y = Math.random() * canvas.height;
+    this.history = [
+      {
+        x: this.x,
+        y: this.y,
+      },
+    ];
+    this.timer = 0;
   }
 }
 
